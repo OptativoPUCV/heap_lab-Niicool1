8c373c23548fa3333710b2295737804d676519d8
repl.it user <replituser@example.com>
2020-10-22 21:12:06 +0000

    new try

--- heap.c
+++ heap.c
@@ -44,7 +44,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
-    Heap *aux;
+    heapElem *aux;
     aux= pq->heapArray[0].data;
     pq->heapArray[0].data= pq->heapArray[pq->size-1].data;
     pq->heapArray[pq->size-1].data= aux;
